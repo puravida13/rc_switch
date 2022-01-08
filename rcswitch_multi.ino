401fb6ffad3c03577c363854f0209b49bb8e7333
puravida13 <50199590+puravida13@users.noreply.github.com>
2022-01-08 14:33:02 -0500

    Update rcswitch_multi.ino

--- rcswitch_multi.ino
+++ rcswitch_multi.ino
@@ -5,7 +5,7 @@ outputs.  When it reads a pwm value greater than 1200 and less than 1800 output 
 and less than 2100 it keeps output 1 on and turns on output 2.
 The declared input and output pins are what i used, and it works on an attiny 85, but you can change as you need. I wired the mosfet source to ground, the gate
 to the tiny85 output pin with a 1k resistor, and the source to whatever device your using.  ground switched.  Positive will be connected constant.
-
+V1.2
 Alex Arocha 2021
 */
 
